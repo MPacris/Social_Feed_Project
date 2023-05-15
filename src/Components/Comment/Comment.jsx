688543c981cbee4e484eed1778b7753efe10ca26
MPacris <mpacris@yahoo.com>
2023-05-15 16:21:21 -0500

    added like and dislike button and counter to comment component

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
+
 
 const Comment = (props) => {
+
+
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+  
+    const HandleLike = () => {
+      setLikes(likes + 1);
+    };
+  
+    const HandleDislike = () => {
+      setDislikes(dislikes + 1);
+    };
+  
+  
+
+
   return ( 
     <table className="table">
 
@@ -10,6 +29,11 @@ const Comment = (props) => {
             <tr key= {index}>
               <tr><td>{newComment.name}</td></tr>
               <tr><td>{newComment.comment}</td></tr>
+              <td><button onClick={HandleLike}>Like ({likes})</button>
+              <button onClick={HandleDislike}>Dislike ({dislikes})</button></td>
+
+         
+
             </tr>
         );
       })}
